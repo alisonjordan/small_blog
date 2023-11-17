2d216be7cb99ec5b8d5a568e646c795973f87c69
alison <alisonjor@gmail.com>
2023-11-16 21:06:17 -0300

    login and logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,22 @@ use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
+    public function logout(){
+        auth()->logout();
+        return redirect('/')->with('success','You logged out with success!');;
+        }
+
+
+    public function showCorrectPage(){
+       if ( auth()->check()) {
+        return view('userhomepage');
+       } else {
+        return view('homepage');
+       }
+       
+    }
+
+
     public function register(Request $request){
         $incomingFields = $request->validate([
             'username' => ['required','min:3','max:10',Rule::unique('users', 'username')],
@@ -15,7 +31,22 @@ class UserController extends Controller
             'password' => ['required','min:8', 'confirmed'],
         ]);
 
-        User::create($incomingFields);
-        return "Hello, welcome to our blog";
+        $user = User::create($incomingFields);
+        auth()->login($user);
+        return redirect('/')->with('success','Thank you for creating an account!');
     }
+
+    public function login(Request $request){
+        $incomingFields = $request->validate([
+            'loginusername' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        if(auth()->attempt(['username'=>$incomingFields['loginusername'],'password'=>$incomingFields['loginpassword']])){
+        $request->session()->regenerate();
+        return redirect('/')->with('success','You logged in with success!');
+    } else{
+        return redirect('/')->with('failure','Username or Password is invalid!');;
+    }
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('homepage');
-});
-
 
 Route::get('/about', function () {
     return view('about');
 });
 
+Route::get('/', [UserController::class, 'showCorrectPage']);
+
 Route::post('/register', [UserController::class, 'register']);
+
+Route::post('/login', [UserController::class, 'login']);
+
+Route::post('/logout', [UserController::class, 'logout']);
