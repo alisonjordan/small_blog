f35ab9e861d4143190b50028e245248eb1774582
Alison Jordan <alisonjor@gmail.com>
2023-12-26 15:01:58 -0300

    just a little comments

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
-
+//SITE RELATED ROUTES
 Route::get('/about', function () {
     return view('about');
 });
 
+//ADMIN RELATED ROUTES
 Route::get('/admin-only', function () {
     return view('about');    
 })->middleware('can:visitAdminPages');
 
+
+//USER RELATED ROUTES
 Route::get('/', [UserController::class, 'showCorrectPage'])->name('login');
 Route::post('/register', [UserController::class, 'register'])->middleware('guest');
 Route::post('/login', [UserController::class, 'login'])->middleware('guest');
@@ -21,7 +24,7 @@ Route::get('/profile/{user:username}', [UserController::class, 'showProfile']);
 Route::get('/manage-avatar', [UserController::class, 'showAvatarManageForm'])->middleware('auth');
 Route::post('/manage-avatar', [UserController::class, 'storeAvatar'])->middleware('auth');
 
-
+//POST RELATED ROUTES
 Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware('auth');
 Route::post('/create', [PostController::class, 'storeNewPost'])->middleware('auth');
 Route::get('/post/{content}', [PostController::class, 'showSinglePost']);
