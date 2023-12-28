7d4e10cc48737537c84daa17f1f5df03581cd867
Alison Jordan <alisonjor@gmail.com>
2023-12-28 14:32:29 -0300

    adding doctitle

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,7 +3,7 @@
     @foreach ($posts as $post)
     <a href="/post/{{$post->id}}" class="list-group-item list-group-item-action">
       <img class="avatar-tiny" src="{{$post->authorPost->avatar}}" />
-      <strong>{{$post->title}}</strong> {{$post->created_at->format('j/n/y')}}
+      <strong>{{$post->title}}</strong> on {{$post->created_at->format('j/n/y')}}
     </a>
     @endforeach
   </div>
